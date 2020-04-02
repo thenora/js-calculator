3c115f7ae4967244feca44297f6e4c4ddf389231
thenora <thenora@gmail.com>
2020-04-02 15:43:43 -0700

    working on switch/case statement

--- calculator.js
+++ calculator.js
@@ -4,6 +4,12 @@ const exampleAdditionInput = {
   operation: 'add',
 }
 
+const exampleSubtractionInput = {
+  num1: 5,
+  num2: 3,
+  operation: 'subtract',
+}
+
 /* The program should have support for these four operations: addition, subtraction, multiplication, and division.
 The program should accept both the name and the symbol for each possible operation. The expected names and symbols for each operation are the following:
 add and +
@@ -19,35 +25,62 @@ check if the input is invalid. What happens if the user input is nil (i.e., the 
 if the input is invalid, output a message to the command line that informs the user that the input was invalid
 */
 
-const validInput = function() {
+const validInput = function(input) {
 
+  return calculator(input)
+}
+
+const calcMath = {
+  add(num1, num2) {
+    return console.log('${num1} + ${num2} = ${num1 + num2}');
+  },
+
+  subtract(num1, num2) {
+    console.log('${num1} - ${num2} = ${num1 - num2}');
+  },
+
+  multiply(num1, num2) {
+    console.log('${num1} * ${num2} = ${num1 * num2}')
+  },
+
+  divide(num1, num2) {
+    console.log('${num1} / ${num2} = ${num1 / num2}')
+  },
 }
 
 
-const calculate = function(input) {
-  validInput()
-  switch (input) {
-    case 3:
-      alert( 'Too small' );
+const calculator = function(input) {
+  // validInput()
+  const number1 = input.num1
+  const number2 = input.num2
+
+  switch (input.operation) {
+    case 'add':
+    case '+':
+      calcMath.add(number1, number2);
       break;
-    case 4:
-      alert( 'Exactly!' );
+    case 'subtract': 
+    case '-':
+      calcMath.subtract(num1, num2);
       break;
-    case 5:
-      alert( 'Too large' );
+    case 'multiply':
+    case '*':
+      calcMath.multiply(num1, num2);
       break;
+    case 'divide':
+    case '/':
+      calcMath.divide(num1, num2);
+      break;
+    case '':
+      console.log('Please tell me to add (+), subtract (-), multiply (*), or divide (/).')
     default:
-      alert( "I don't know such values" );
-  }
+      console.log('Sorry, I don\'t know what to do with "${operation}". Please tell me to add, subtract, multiply, or divide.');
+  };
 }
 
 
 
-calculate(exampleAdditionInput) {
-
-}
 
 
-
-// There is a switch statement in JavaScript
+calculator(exampleAdditionInput)
 
